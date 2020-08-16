a2098ccdb6b5003cd671db5aa7d59b633ed3a8ae
Shakirul Islam Leeon <64961610+shakirul360@users.noreply.github.com>
2020-08-16 23:19:06 +0600

    Linked List Implementation with head as None
    The Linked list contains functions - insert, insert at beginning, size, pop, remove, display and get array from list.
    #Descriptions of what each function does is written in comments in the code.

---
+++ Linked List Python Implementation with head as None.py
@@ -0,0 +1,89 @@
+#By Shakirul Islam Leeon
+#git - Shakirul360
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.count = 0   
+#Append function adds checks if the list is empty and adds data to the list however needed.    
+    def append(self,data):
+        if self.head == None:
+            self.head = Node(data)
+            self.count += 1
+        else:
+            cur = self.head
+            while cur.next != None:
+                cur = cur.next
+            cur.next = Node(data)
+            self.count += 1
+            
+#Size function returns the size of the list  
+    def size(self):
+        return self.count
+    
+#insert_at_beginning function adds a node at the start of the list and calls it head of the list    
+    def insert_at_beginning(self,data):
+        if self.head == None:
+            self.head = Node(data)
+            self.count += 1
+        else:
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
+            self.count += 1
+            
+#get_array_from_list returns an array created from the data values of the list    
+    def get_array_from_list(self):
+        arr = []
+        cur = self.head
+        arr.append(self.head.data)
+        while cur.next != None:
+            arr.append(cur.next.data)
+            cur = cur.next
+        return arr
+    
+#display function prints the data values of the nodes in the list     
+    def display(self):
+        cur = self.head
+        print(cur.data, end = ' ')
+        while cur.next:
+            cur = cur.next
+            print(cur.data,end = ' ')
+            
+#is_empty function checks if the list is empty and replies accordingly    
+    def is_empty(self):
+        if self.count == 0:
+            return True
+        return False
+    
+# remove function removes a node from the list, given the value to be removed.    
+    def remove(self,data):
+        cur = self.head.next
+        prev = self.head
+        while prev.data != data and cur.next != None:
+            prev = cur
+            cur = cur.next
+        prev.next = cur.next
+        self.count -= 1
+    
+#pop function removes the last item on the list
+    def pop(self):
+        current_node = self.head
+        while current_node.next:
+            if current_node.next.next == None:
+                current_node.next = None
+            else:
+                current_node = current_node.next
+        
+l = LinkedList()
+n = 5
+for _ in range(n):
+    l.append(input())
+
+l.size()
+    
+            
