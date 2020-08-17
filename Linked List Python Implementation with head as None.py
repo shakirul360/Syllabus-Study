e706ec5fb39b190a06bd73538ea59522962b66f4
Shakirul Islam Leeon <64961610+shakirul360@users.noreply.github.com>
2020-08-17 21:02:31 +0600

    Update Linked List Python Implementation with head as None.py
    #1 A new function added - remove_first.
    #2 Fixed methods where functions would crash if the list was empty.

--- Linked List Python Implementation with head as None.py
+++ Linked List Python Implementation with head as None.py
@@ -2,58 +2,62 @@
 #git - Shakirul360
 
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.next = None
+        
 class LinkedList:
     def __init__(self):
         self.head = None
         self.count = 0   
+        
 #Append function adds checks if the list is empty and adds data to the list however needed.    
-    def append(self,data):
+    def append(self, data):
         if self.head == None:
             self.head = Node(data)
-            self.count += 1
         else:
             cur = self.head
             while cur.next != None:
                 cur = cur.next
             cur.next = Node(data)
-            self.count += 1
+        self.count += 1
             
 #Size function returns the size of the list  
     def size(self):
         return self.count
     
 #insert_at_beginning function adds a node at the start of the list and calls it head of the list    
-    def insert_at_beginning(self,data):
+    def insert_at_beginning(self, data):
         if self.head == None:
             self.head = Node(data)
-            self.count += 1
         else:
             new_node = Node(data)
             new_node.next = self.head
             self.head = new_node
-            self.count += 1
+        self.count += 1
             
 #get_array_from_list returns an array created from the data values of the list    
     def get_array_from_list(self):
-        arr = []
-        cur = self.head
-        arr.append(self.head.data)
-        while cur.next != None:
-            arr.append(cur.next.data)
-            cur = cur.next
-        return arr
-    
+        if self.count == 0:
+            print("No data in list to extract into an array")
+        else:
+            arr = []
+            current_node = self.head
+            while current_node:
+                arr.append(current_node.data)
+                current_node = current_node.next
+            return arr
+        
 #display function prints the data values of the nodes in the list     
     def display(self):
-        cur = self.head
-        print(cur.data, end = ' ')
-        while cur.next:
-            cur = cur.next
-            print(cur.data,end = ' ')
-            
+        if self.count == 0:
+            print("No data to display")
+        else:
+            cur = self.head
+            while cur:
+                print(cur.data, end = ' ')
+                cur = cur.next
+                
 #is_empty function checks if the list is empty and replies accordingly    
     def is_empty(self):
         if self.count == 0:
@@ -61,23 +65,36 @@ class LinkedList:
         return False
     
 # remove function removes a node from the list, given the value to be removed.    
-    def remove(self,data):
-        cur = self.head.next
-        prev = self.head
-        while prev.data != data and cur.next != None:
-            prev = cur
-            cur = cur.next
-        prev.next = cur.next
-        self.count -= 1
+    def remove(self, data):
+        if self.count == 0:
+            print("No data in list to remove")
+        else:
+            cur = self.head.next
+            prev = self.head
+            while prev.data != data and cur.next != None:
+                prev = cur
+                cur = cur.next
+            prev.next = cur.next
+            self.count -= 1
     
 #pop function removes the last item on the list
-    def pop(self):
-        current_node = self.head
-        while current_node.next:
-            if current_node.next.next == None:
-                current_node.next = None
-            else:
-                current_node = current_node.next
+    def remove_last(self):
+        if self.count == 0:
+            print("No Items to remove")
+        else:
+            current_node = self.head
+            while current_node.next:
+                if current_node.next.next == None:
+                    current_node.next = None
+                else:
+                    current_node = current_node.next
+    
+    def remove_first(self):
+        if self.count ==0:
+            print("No Items to remove")
+        else:
+            cur = self.head
+            self.head = cur.next
         
 l = LinkedList()
 n = 5
@@ -85,5 +102,3 @@ for _ in range(n):
     l.append(input())
 
 l.size()
-    
-            
