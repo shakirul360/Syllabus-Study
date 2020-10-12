8c6dad52565136f7bfc5d6733487f22647b3ac91
Shakirul Islam Leeon <shakirul360@gmail.com>
2020-10-13 00:24:13 +0600

    Update Binary Search Tree Implementation INCOMP.py
    Facing problems implementing delete method.

--- Binary Search Trees/Binary Search Tree Implementation INCOMP.py
+++ Binary Search Trees/Binary Search Tree Implementation INCOMP.py
@@ -1,112 +1,209 @@
 class Node:
-    def __init__(self, value):
+    def __init__(self,value):
         self.value = value
-        self.leaf = True
         self.left = None
         self.right = None
     
     def insert(self, value):
         if self.value == value:
             return
-        elif self.value > value:
+        if self.value > value:
             if self.left:
                 return self.left.insert(value)
             else:
                 self.left = Node(value)
-        else:
+        if self.value < value:
             if self.right:
                 return self.right.insert(value)
             else:
                 self.right = Node(value)
     
     def find(self, value):
-        if self.value == value:
+        if self.value == value: #check to see if the current node's value matches the search value. NOTE - initiation self == root.
             found = True
-        elif self.value == None:
-            found = False
-        elif self.value < value:
+        elif self.value < value: #if the value is greater, check the right side of the tree if right side is there.
             if self.right:
                 return self.right.find(value)
             else:
                 found = False
         else:
-            if self.left:
+            if self.left: #if the value is less, check the left side of the tree if left side is there.
                 return self.left.find(value)
             else:
-                found = True     
+                Found = True        
         if found:
-            print("The data value, {}, is in the tree".format(value))
+            print("Value {} in the tree".format(value))
         else:
-            print("The data value, {}, is not in the tree".format(value))
-        
-    def remove(self, value):
-        if self.value == value:
-
-            if self.right and self.left:
-                left_child = self.left
-                right_child = self.right
-                cur = self.right
-                while cur.left != None:
-                    cur = self.left
-                self.value = cur.value
-                self.right = right_child
-                self.left = left_child    
-            elif self.right:
-                cur = self.right
-                self.value = cur.value
-            elif self.left:
-                cur = self.left
-                self.value = cur.left
-            else:
+            print("value {} not in the tree".format(value))
+            
+    def pre_order(self):
+        if self and self != None:
+            print(self.value)
+        if self.left:
+            self.left.pre_order()
+        if self.right:
+            self.right.pre_order()
+            
+    def in_order(self):
+        if self:
+            if self.left:
+                self.left.in_order()
+            if self != None:
+                print(self.value)
+            if self.right:
+                self.right.in_order()
+            
+    def post_order(self):
+        if self:
+            if self.left:
+                self.left.post_order()
+            if self.right:
+                self.right.post_order()
+        if self != None:
+            print(self.value)
+            
+    def delete(self, value):
+        if self.value == value: #matches the node's value and then checks to see if it has both left or right, one of them or none.
+            if not self.left and not self.right:        #if it has no nodes connected to it
                 self.value = None
-        elif self.value == None:
-            return
-        elif self.value < value:
-            return self.right.remove(value)
-        elif self.value > value:
-            return self.left.remove(value)
-        
-        
-class Graph:
+    
+class Tree:
     def __init__(self):
         self.root = None
-        self.arr = []
     
     def insert(self, value):
-        if self.root:
-            return self.root.insert(value)
-        else:
+        if self.root == None:
             self.root = Node(value)
-
+        else:
+            return self.root.insert(value)
+        
     def find(self, value):
-        if self.root:
-            return self.root.find( value)
+        if self.root == None:
+            print("No tree to find values on")
         else:
-            print("No data in tree to check")
+            return self.root.find(value)
     
-    def remove(self, value):
+    def pre_order(self):
+        print("1....2...3...Pre Order Traversal initiated")
+        self.root.pre_order()
+    
+    def in_order(self):
+        print("1....2...3...In Order Traversal initiated")
+        self.root.in_order()
+    
+    def post_order(self):
+        print("1....2...3...Post Order Traversal initiated")
+        self.root.post_order()
+    
+    def delete(self, value):
         if self.root:
-            return self.root.remove(value)
+            if self.root.value == value:     # if the delete value is at the root.
+                if not self.root.left and not self.root.right:
+                    self.root.value = None
+                elif self.root.left and not self.root.right:
+                    self.root = self.root.left
+                elif not self.root.left and self.root.right:
+                    self.root = self.root.right
+                
+                elif self.root.left and self.root.right:
+                    delNodeParent = self.root
+                    delNode = self.root.right
+                    while delNode.left:
+                        delNodeParent = delNode
+                        delNode = delNode.left
+                    
+                    self.root.value = delNode.value
+                    if delNode.right:
+                        if delNodeParent.value > delNode.value:
+                            delNodeParent.left = delNode.right
+                        elif delNodeParent.value < delNode.value:
+                            delNodeParent.right = delNode.right
+                        else:
+                            if delNode.value < delNodeParent.value:
+                                delNodeParent.left = None
+                            else:
+                                delNodeParent.right = None
+                
+            else:
+                if self.root.value < value:
+                    delnode = self.root.right
+                    delnodeparent = self.root
+                if self.root.value > value:
+                    delnode = self.root.left
+                    delnodeparent = self.root
+                    
+                    while delnode.value != value:
+                        if delnode.value > value:
+                            delnode = delnode.left
+                            delnodeparent = delnode
+                        else:
+                            delnode = delnode.right
+                            delnodeparent = delnode
+                    
+                    if not delnode.left and not delnode.right:
+                        if delnodeparent.value > delnode.value:
+                            delnodeparent.left = None
+                        else:
+                            delnodeparent.right = None
+        
+                    elif delnode.left and not delnode.right:
+                        if value < delnodeparent.value:
+                            delnodeparent.left = delnode.left
+                        else:
+                            delnodeparent.right = delnode.left
+                    
+                    elif not delnode.left and delnode.right:
+                        if data < parent.value:
+                            parent.left = node.right
+                        else:
+                            parent.right = node.right
+                        
+                    else: #if both left and right children
+                        node = delnode #keeping a copy of the node to be replaced
+                        while delnode.left:
+                            delnodeparent = delnode
+                            delnode = delnode.left
+                        node.value - delnode.value
+                        if delnode.right:
+                            if delnodeparent.value > delnode.value:
+                                delnodeparent.left = delnode.right
+                            elif delnodeparent.value < delnode.value:
+                                delnodeparent.right = delnode.right
+                        else:
+                            if delnode.value < delnodeparent.value:
+                                delnodeparent.left = None
+                            else:
+                                delnodeparent.right = None                
+                    
         else:
-            print("No data to remove from")
-            
-g = Graph()
-g.insert(20)
-g.insert(30)
-g.insert(23)
-g.insert(33)
-g.insert(10)
+            print("no tree to remove value from")
 
-g.find(30)
-g.remove(30)
-g.find(30)
-g.find(10)
-g.remove(10)
-g.find(10)
-g.find(23)
-g.remove(23)
-g.find(23)
-#g.find(60)
-#g.find(50)
-#g.find(40)
-#g.find(70)
+            
+        
+t = Tree()
+t.insert(50)
+t.insert(30)
+t.insert(70)
+t.insert(20)
+t.insert(40)
+t.insert(60)
+t.insert(80)
+print("PRE DELETE")
+print(" ")
+t.in_order()
+print(" ")
+t.pre_order()
+print(" ")
+t.post_order()
+print(" ")
+
+t.delete(50)
+t.in_order()
+print("POST delete of 50, the root")
+t.insert(50)
+t.delete(70)
+t.in_order()
+print("POST delete of 70, a node with both left and right childs")
+t.insert(10)
+t.delete(10)
+t.post_order()
