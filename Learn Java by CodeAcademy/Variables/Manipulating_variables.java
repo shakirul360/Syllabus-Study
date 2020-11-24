1bcd61b6100848699c544000c75b0108ecf31d31
Shakirul Islam Leeon <shakirul360@gmail.com>
2020-11-25 02:49:18 +0600

    Java variables

---
+++ Learn Java by CodeAcademy/Variables/Manipulating_variables.java
@@ -0,0 +1,13 @@
+public class BankAccount {
+    public static void main(String[] args){
+    double balance = 1000.75;
+    double amountToWithdraw = 250;
+    double updatedBalance = balance - amountToWithdraw;
+    double amountForEachFriend = updatedBalance / 3;
+    boolean canPurchaseTicket = amountForEachFriend >= 250;
+    System.out.println(canPurchaseTicket);
+    String message = "I gave each friend" + " " + amountForEachFriend + "...";
+    System.out.println(message);
+
+  }       
+}
---
+++ Learn Java by CodeAcademy/Variables/Variables.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+    String name = "Leon";
+    int age = 21;
+    double desiredSalary = 23.22;
+    char gender = 'm';
+    boolean lookingForJob = true;
+	}
+}
